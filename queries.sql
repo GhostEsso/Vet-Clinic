3f7733d8c619e5265e37ea8b2c63f2879cc28514
GhostEsso <lilipitaham@gmail.com>
2023-09-19 14:46:07 +0000

    analytical queries added

--- queries.sql
+++ queries.sql
@@ -23,3 +23,37 @@ SELECT * FROM animals WHERE name <> 'Gabumon';
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (inclusive).
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- Transaction 1: Update column "species" to "unspecified"
+BEGIN;
+UPDATE animals SET species = 'unspecifed';
+SELECT * FROM animals; -- verification
+ROLLBACK; -- Cancel transaction
+
+-- Transaction 2: Update of the "species" column for certain animals
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals; -- verification
+COMMIT; -- commit transaction_timestamp
+
+-- Transaction 3: Delete all records
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals; -- Verification
+ROLLBACK; --Cancel transaction_timestamp
+
+-- Transaction 4: Remove animals born after January 1, 2022
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SELECT * FROM animals; -- Verification
+COMMIT; -- Commit transaction
+
+-- Analytical queries
+SELECT COUNT(*) FROM animals; -- How many animals are there?
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0; -- Animals that have never attempted to escape
+SELECT AVG(weight_kg) FROM animals; -- Average weight of animals
+SELECT neutered, COUNT(*) FROM animals GROUP BY neutered; -- Who escapes the most, castrated animals or not?
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species; -- Minimum and maximum weight of each type of animal
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'; -- Average number of escape attempts by type of animal born between 1990 and 2000
+
