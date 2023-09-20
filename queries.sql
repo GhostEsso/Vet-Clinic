eb7995ac2daef10fd878f4488669733eff9dae33
GhostEsso <lilipitaham@gmail.com>
2023-09-20 20:41:02 +0000

    errors fixed

--- queries.sql
+++ queries.sql
@@ -65,47 +65,48 @@ SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
     WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
     GROUP BY species;
 
--- Animals belong to Melody Pond
-SELECT a.name AS animal_name
-FROM animals a 
-JOIN owners o ON a.owner_id = o.id 
+-- What animals belong to Melody Pond?
+SELECT a.name
+FROM animals a
+JOIN owners o ON a.owner_id = o.id
 WHERE o.full_name = 'Melody Pond';
 
--- List of all animals that are pokemon (their type is Pokemon).
-SELECT animals.name
-FROM animals
-JOIN species ON animals.species = species.name
-WHERE species.name = 'Pokemon';
+-- List of all animals that are pokemon (their species is Pokemon).
+SELECT a.name
+FROM animals a
+JOIN species s ON a.species_id = s.id
+WHERE s.name = 'Pokemon';
 
-
--- List all owners and their animals
-SELECT o.full_name AS owner_name, a.name AS animal_name
+-- List all owners and their animals, including those who don't own any animal.
+SELECT o.full_name, COALESCE(a.name, 'No animal') AS animal
 FROM owners o
 LEFT JOIN animals a ON o.id = a.owner_id;
 
 -- How many animals are there per species?
-SELECT s.name AS species_name, COUNT(*) AS animal_count
+SELECT s.name, COUNT(*) AS animal_count
 FROM animals a
 JOIN species s ON a.species_id = s.id
 GROUP BY s.name;
 
 -- List all Digimon owned by Jennifer Orwell.
-SELECT animals.name
-FROM animals
-JOIN species ON animals.species = species.name
-JOIN owners ON animals.owner_id = owners.id
-WHERE species.name = 'Digimon' AND owners.full_name = 'Jennifer Orwell';
+SELECT a.name
+FROM animals a
+JOIN species s ON a.species_id = s.id
+JOIN owners o ON a.owner_id = o.id
+WHERE o.full_name = 'Jennifer Orwell'
+AND s.name = 'Digimon';
 
 -- List all animals owned by Dean Winchester that haven't tried to escape.
-SELECT a.name AS animal_name
+SELECT a.name
 FROM animals a
 JOIN owners o ON a.owner_id = o.id
-WHERE o.full_name = 'Dean Winchester' AND a.escape_attempts = 0;
+WHERE o.full_name = 'Dean Winchester'
+AND a.escape_attempts = 0;
 
 -- Who owns the most animals?
-SELECT o.full_name AS owner_name, COUNT(a.id) AS animal_count
+SELECT o.full_name, COUNT(*) AS animal_count
 FROM owners o
-LEFT JOIN animals a ON o.id = a.owner_id
+JOIN animals a ON o.id = a.owner_id
 GROUP BY o.full_name
-ORDER BY COUNT(a.id) DESC
-LIMIT 1;
+ORDER BY animal_count DESC
+LIMIT 1;                                    
--- schema.sql
+++ schema.sql
@@ -1,39 +1,40 @@
-/* Database schema to keep the structure of the entire database. */
-
--- Create the vet_clinic database
+-- vetclinic database
 CREATE DATABASE vet_clinic;
--- Create the animals' table
+
+-- animals table
 CREATE TABLE animals (
-  id INT PRIMARY KEY,
-  name VARCHAR(255),
-  date_of_birth DATE,
-  escape_attempts INTEGER,
-  neutered BOOLEAN,
-  weight_kg DECIMAL
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    date_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weight_kg DECIMAL,
+    species VARCHAR(255), -- 'species column'
+    species_id INTEGER, -- 'species_id column'
+    owner_id INTEGER -- 'owner_id column'
 );
 
+-- foreign key
 ALTER TABLE animals
-ADD COLUMN owner_id INT,
-ADD CONSTRAINT fk_owner
-  FOREIGN KEY (owner_id)
-  REFERENCES owners (id);
-
-ALTER TABLE animals
-ADD COLUMN species_id INT,
 ADD CONSTRAINT fk_species
-  FOREIGN KEY (species_id)
-  REFERENCES species (id);
+FOREIGN KEY (species_id)
+REFERENCES species(id);
 
+-- foreign key
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
 
--- The owners table pg_create_restore_point
+-- owner's table
 CREATE TABLE owners (
-  id SERIAL PRIMARY KEY,
-  full_name VARCHAR(255),
-  age INTEGER
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100),
+    age INTEGER
 );
 
--- Create the species TABLE
+-- species table
 CREATE TABLE species (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
 );
