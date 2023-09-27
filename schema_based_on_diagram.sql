549050797dc45a8cc8f34c171fa0b8d92f58afd9
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-09-27 12:30:20 +0430

    Add medical table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,5 +1,13 @@
 CREATE TABLE patients (
-    id PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(30),
     date_of_birth DATE
 );
+
+CREATE TABLE medical_histories (
+    id SERIAL PRIMARY KEY,
+    admitted_at TIMESTAMP, 
+    patient_id FOREIGN KEY, 
+    status VARCHAR(20)
+);
+
